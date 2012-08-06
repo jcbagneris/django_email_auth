0f31b391937c5fc3d95a7fde4459fee2592347b4
JcB <jcbagneris@gmail.com>
2012-08-06 12:05:57 +0200

    tests: create func for forms doctests

--- email_auth/tests.py
+++ email_auth/tests.py
@@ -1,70 +1,71 @@
 # -*- encoding: utf-8 -*-
 from django.test import TestCase
 
-"""
-Forms tests
+def test_forms():
+    """
+    Forms doctests
+    >>> from django.conf import settings
 
->>> from django.conf import settings
+    # Ugly, but necessary for testing purposes
+    >>> settings.CUSTOM_USER_MODEL = None
 
-# Ugly, but necessary for testing purposes
->>> settings.CUSTOM_USER_MODEL = None
+    >>> from django.contrib.auth.models import User
+    >>> from email_auth.forms import AuthenticationForm
+    >>> user = User.objects.create_user("jsmith", "jsmith@example.com", "test123")
 
->>> from django.contrib.auth.models import User
->>> from email_auth.forms import AuthenticationForm
->>> user = User.objects.create_user("jsmith", "jsmith@example.com", "test123")
+    # The user cannot log in with username, even correct
 
-# The user cannot log in with username, even correct
+    >>> data = {
+    ...     'username': 'jsmith',
+    ...     'password': 'test123',
+    ... }
 
->>> data = {
-...     'username': 'jsmith',
-...     'password': 'test123',
-... }
+    >>> form = AuthenticationForm(None, data)
+    >>> form.is_valid()
+    False
+    >>> form.non_field_errors()
+    [u'Please enter a correct email and password. Note that both fields are case-sensitive.']
 
->>> form = AuthenticationForm(None, data)
->>> form.is_valid()
-False
->>> form.non_field_errors()
-[u'Please enter a correct email and password. Note that both fields are case-sensitive.']
+    # The user submits an invalid email.
 
-# The user submits an invalid email.
+    >>> data = {
+    ...     'email': 'jsmith',
+    ...     'password': 'test123',
+    ... }
 
->>> data = {
-...     'email': 'jsmith',
-...     'password': 'test123',
-... }
+    >>> form = AuthenticationForm(None, data)
+    >>> form.is_valid()
+    False
+    >>> form.non_field_errors()
+    [u'Please enter a correct email and password. Note that both fields are case-sensitive.']
 
->>> form = AuthenticationForm(None, data)
->>> form.is_valid()
-False
->>> form.non_field_errors()
-[u'Please enter a correct email and password. Note that both fields are case-sensitive.']
+    # The user is inactive.
 
-# The user is inactive.
+    >>> data = {
+    ...     'email': 'jsmith@example.com',
+    ...     'password': 'test123',
+    ... }
+    >>> user.is_active = False
+    >>> user.save()
+    >>> form = AuthenticationForm(None, data)
+    >>> form.is_valid()
+    False
+    >>> form.non_field_errors()
+    [u'This account is inactive.']
 
->>> data = {
-...     'email': 'jsmith@example.com',
-...     'password': 'test123',
-... }
->>> user.is_active = False
->>> user.save()
->>> form = AuthenticationForm(None, data)
->>> form.is_valid()
-False
->>> form.non_field_errors()
-[u'This account is inactive.']
+    >>> user.is_active = True
+    >>> user.save()
 
->>> user.is_active = True
->>> user.save()
+    # The success case
 
-# The success case
+    >>> form = AuthenticationForm(None, data)
+    >>> form.is_valid()
+    True
+    >>> form.non_field_errors()
+    []
 
->>> form = AuthenticationForm(None, data)
->>> form.is_valid()
-True
->>> form.non_field_errors()
-[]
-
-"""
+    """
+    pass
 
 from django.conf import settings
 from django.contrib.auth.models import User
